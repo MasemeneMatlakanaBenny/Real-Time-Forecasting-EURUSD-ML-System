b5cd58f0723254c8b5bcf1b90d875aba03a1642f
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-17 21:39:37 +0200

    extract data using yfinance

--- src/etl_workflow/phases/extract.py
+++ src/etl_workflow/phases/extract.py
@@ -1,1 +1,15 @@
+import yfinance as yf
+import pandas as pd
 
+
+# Download data
+df = yf.download(
+    tickers="EURUSD=X",
+    period="30d",
+    interval="1h",
+    auto_adjust=False,
+    progress=False
+)
+
+## save the df as csv file:
+df.to_csv("data/extracted_df.csv",index=False)
