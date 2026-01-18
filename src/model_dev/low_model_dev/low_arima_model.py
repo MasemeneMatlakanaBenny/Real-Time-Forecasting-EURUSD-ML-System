22250ef1e8fe6f70e9d2d25d18774f5c1c178d09
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-18 21:36:38 +0200

    Update low_arima_model.py

--- src/model_dev/low_model_dev/low_arima_model.py
+++ src/model_dev/low_model_dev/low_arima_model.py
@@ -1,1 +1,20 @@
+import pandas as pd
+import joblib
+from statsmodels.tsa.arima.model import ARIMA
+
+## load the transformed dataset:
+df=pd.read_csv("data/transformed_df.csv")
+
+## save the datetime and high only:
+df=df[['Datetime','Low']]
+
+## develop the model:
+model=ARIMA(df['Low'],order=(1,0,0))
+
+## fit the model:
+model_fitted=model.fit()
+
+## save the model as pickle file:
+joblib.dump(model,"models/low_models/low_arima_model.pkl")
+
 
