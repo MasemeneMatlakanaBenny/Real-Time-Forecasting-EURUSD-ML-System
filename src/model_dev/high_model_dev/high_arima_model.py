c96da6876905434962afb9281e37f249b0de361c
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-18 21:36:19 +0200

    Update high_arima_model.py

--- src/model_dev/high_model_dev/high_arima_model.py
+++ src/model_dev/high_model_dev/high_arima_model.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import joblib
+from statsmodels.tsa.arima.model import ARIMA
+
+## load the transformed dataset:
+df=pd.read_csv("data/transformed_df.csv")
+
+## save the datetime and high only:
+df=df[['Datetime','High']]
+
+## develop the model:
+model=ARIMA(df['High'],order=(1,0,0))
+
+## fit the model:
+model_fitted=model.fit()
+
+## save the model as pickle file:
+joblib.dump(model,"models/high_models/high_arima_model.pkl")
 
