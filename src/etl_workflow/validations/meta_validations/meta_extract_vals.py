9176ba37a426f7eec3da2b040b7bb78b9280e31f
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2026-01-19 15:31:39 +0200

    Update meta_extract_vals.py

--- src/etl_workflow/validations/meta_validations/meta_extract_vals.py
+++ src/etl_workflow/validations/meta_validations/meta_extract_vals.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from prefect import task,flow
+from lib.validations import *
 
+## load the extract validations dataframe results that was saved in a csv format:
+results_df=pd.read_csv("data_quality_checks/extract_checks_df.csv")
+
+## create the batch first:
+batch=create_batch(df=results_df)
+
+## run the meta validations:
+checks_results=meta_validation(batch=Batch,expectation_results=results_df)
+
+
+## print the checks results-if success then data quality checks passed
+print(checks_results)
